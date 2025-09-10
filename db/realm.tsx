ef42d07469e2193aa9913bf711bb3837c936fb42
Daniel Pedersoli <dpmsengineer@gmail.com>
2025-09-10 17:06:01 -0300

    feature: migration is working fine and new running script was added to run on clean android environment

--- db/realm.tsx
+++ db/realm.tsx
@@ -1,32 +1,32 @@
 import React, { ReactNode } from 'react';
 import { createRealmContext } from '@realm/react';
 import { Task } from './schemas';
-import { Realm as RealmTypes } from 'realm';
+import Realm from 'realm';
 
-export const CURRENT_REALM_SCHEMA_VERSION = 2;
+export const CURRENT_REALM_SCHEMA_VERSION = 4;
 
-const realmConfig = {
-  schema: [Task],
-  // deleteRealmIfMigrationNeeded: true, // somente para dev ou ambientes que não precisa persistir os dados pós "migration"
-  schemaVersion: CURRENT_REALM_SCHEMA_VERSION,
-  migration: (oldRealm: RealmTypes, newRealm: RealmTypes) => {
-    if (oldRealm.schemaVersion < CURRENT_REALM_SCHEMA_VERSION) {
-      const oldTasks = oldRealm.objects<Task>('Task');
-      const newTasks = newRealm.objects<Task>('Task');
+const migrationFunction = (oldRealm: Realm, newRealm: Realm) => {
+  if (oldRealm.schemaVersion < CURRENT_REALM_SCHEMA_VERSION) {
+    const oldTasks = oldRealm.objects<Task>('Task');
+    const newTasks = newRealm.objects<Task>('Task');
 
-      for (let i = 0; i < oldTasks.length; i++) {
-        // Copia os dados existentes e define valor padrão para a nova propriedade
-
-        newTasks[i].title = 'MIGROOOOOU O REALM!!!'; // ou null, ou qualquer valor default
-      }
+    for (let i = 0; i < oldTasks.length; i++) {
+      // newTasks[i].title = 'MIGROOOOOU O REALM!!! para v4';
     }
-  },
+  }
 };
 
-const { RealmProvider, useRealm, useQuery, useObject } = createRealmContext(realmConfig);
+const { RealmProvider, useRealm, useQuery, useObject } = createRealmContext();
 
 export { useRealm, useQuery, useObject };
 
 export function RealmWrapper({ children }: { children: ReactNode }) {
-  return <RealmProvider>{children}</RealmProvider>;
+  return (
+    <RealmProvider
+      schema={[Task]}
+      schemaVersion={CURRENT_REALM_SCHEMA_VERSION}
+      onMigration={migrationFunction}>
+      {children}
+    </RealmProvider>
+  );
 }
